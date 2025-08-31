2dfd52fe1cc5790fc22750a0a675071f0fd53ba2
Rajnish Kumar <rajnishroushan2020@gmail.com>
2025-08-31 20:09:44 +0530

    revised the code

--- basic_2/array.js
+++ basic_2/array.js
@@ -1,15 +1,15 @@
-// let arr = [1, 2, 3, 4]; // method 1
-// console.log(arr);
+let arr = [1, 2, 3, 4]; // method 1
+console.log(arr);
 
-// let arr1 = ["rajnish", 1, 3, true] // array can contain any values
-// console.log(arr1);
-// console.log(arr1[3]);
+let arr1 = ["rajnish", 1, 3, true] // array can contain any values
+console.log(arr1);
+console.log(arr1[3]);
 
 
-// let newArr = new Array(1, 22, 23, 4, 5); // method 2
-// console.log(newArr);
+let newArr = new Array(1, 22, 23, 4, 5); // method 2
+console.log(newArr);
 
-// console.log(newArr.length); // length of arrray
+console.log(newArr.length); // returns the length of arrray
 
 
 // ********************* METHODS IN ARRAY *****************************
@@ -17,24 +17,24 @@
 let arry2 = new Array(55, 66, 55, 88, 96, 69);
 console.log(arry2);
 
-// arry2.push(8) // inserts the new value at last .
-// console.log(arry2);
+arry2.push(8) // inserts the new value at last.
+console.log(arry2);
 
-// arry2.push(96)
-// console.log(arry2);
+arry2.push(96)
+console.log(arry2);
 
-// arry2.pop() // removes the last value.
-// console.log(arry2);
+arry2.pop() // removes the last value.
+console.log(arry2);
 
-// arry2.unshift(9) // adds the value in starting 
-// console.log(arry2);
+arry2.unshift(9) // adds the value in starting 
+console.log(arry2);
 
-// arry2.shift() // removes the first value
-// console.log(arry2);
+arry2.shift() // removes the first value
+console.log(arry2);
 
-// console.log(arry2.join()); // converts the array in to string;
+console.log(arry2.join()); // converts the array in to string;
 
-// console.log(arry2.toString()); // converts the array in to string;
+console.log(arry2.toString()); // converts the array in to string;
 
 
 // ************************ Slice and Splice (INTERVIEW QUESTION ) *****************************
