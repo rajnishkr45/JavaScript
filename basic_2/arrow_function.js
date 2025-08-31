84a3bba1581b72239d34cf6c1b7ed5d4dedafc99
Rajnish Kumar <rajnishroushan2020@gmail.com>
2025-08-31 20:35:15 +0530

    revised functions

--- basic_2/arrow_function.js
+++ basic_2/arrow_function.js
@@ -1,10 +1,10 @@
 // ------------------- Simple Function -----------
 
-// const addOne = function (num1) {
-//     return num1 + 1;
-// }
+const addOne = function (num1) {
+    return num1 + 1;
+}
 
-// console.log(addOne(5));
+console.log(addOne(5));
 
 // --------------- Arrow function --------------------
 
@@ -12,14 +12,14 @@ const addTwo = (num2) => {
     return num2 + 2;
 } // when we use curly braces then return must be written 
 
-const addThree = (num1, num2, num3) => (num1 + num2 + num3) // this is also a way to write arraow function and it will abe used in react 
+const addThree = (num1, num2, num3) => (num1 + num2 + num3) // this is also a way to write arraow function and it will be used in react 
 
-// const users = {
-//     username: "Rajnish",
-//     price: 999,
+const users = {
+    username: "Rajnish",
+    price: 999,
 
-//     welcomeMessage: function () {
-//         console.log(`Hello {username}`)
-//     }
+    welcomeMessage: function () {
+        console.log(`Hello ${username}`)
+    }
 
-// }
+}
--- basic_2/function1.js
+++ basic_2/function1.js
@@ -19,24 +19,24 @@ function myName() {
 
 myName // it is called as refrence 
 
-// myName() // it is exicuation when we use () then it is called as exicuation
+myName() // it is exicuated when we use () then it is called as exicuation
 
 
-// function addTwoNumbers(num1, num2) {
-//     console.log(num1 + num2);
-// }
+function addTwoNumbers(num1, num2) {
+    console.log(num1 + num2);
+}
 
 // addTwoNumbers() // value needed in the parameters to give the answer
-// addTwoNumbers(5,4)
-// addTwoNumbers(5, "4") // it will add number and string by considerig as string and keep it side wise as 54
-// addTwoNumbers(5, "a") // it will add number and string by considerig as string and keep it side wise as 54
-// addTwoNumbers(5, null) 
+console.log(addTwoNumbers(5, 4))
+console.log(addTwoNumbers(5, "4")); // it will add number and string by considerig as string and keep it side wise as 54
+console.log(addTwoNumbers(5, "a"))// it will add number and string by considerig as string and keep it side wise as 54
+console.log(addTwoNumbers(5, null))
 
 // let result = addTwoNumbers(6, 9)
 // console.log(result)  // it will give result as undefined as the function is not returning any value 
 
 function addTwoNumbers(num1, num2) {
-    //    let sum= num1+num2
+    //    let sum = num1+num2
     //    return sum 
     return num1 + num2
 }
