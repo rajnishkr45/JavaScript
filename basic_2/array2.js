2fffa386e793822731401221438e853785e9d399
Rajnish Kumar <rajnishroushan2020@gmail.com>
2025-08-31 20:24:03 +0530

    Revised array code

--- basic_2/array2.js
+++ basic_2/array2.js
@@ -3,28 +3,28 @@
 let marvel_Heros = ["thor", "ironman", "dr_strange"]
 let dc_Heros = ["superman", "batman", "flash"]
 
-// marvel_Heros.push(dc_Heros);  // array can contain any value ie. it may be string may be number or may be another array and push method insterts one array in another .
+// marvel_Heros.push(dc_Heros);  // array can contain any value ie. it may be string may be number or may be another array and push method insterts one array in another at last of array.
 
-// console.log(marvel_Heros);
+console.log(marvel_Heros);
 
-// let all_heros = marvel_Heros.concat(dc_Heros); // concat add the TWO array ie. it stores all elements together;
-// console.log(all_heros);
+let all_heros = marvel_Heros.concat(dc_Heros); // concat add the TWO array ie. it stores all elements together;
+console.log(all_heros);
 
-// let all_ne_heros = [...dc_Heros, ...marvel_Heros]  // it is konwn as spread and it works same as concat but main difference is that it can add more tha two array ;
-// console.log(all_ne_heros);
+let all_ne_heros = [...dc_Heros, ...marvel_Heros]  // it is konwn as spread and it works same as concat but main difference is that it can add more tha two array ;
+console.log(all_ne_heros);
 
-// let another_arry = [1, 2, 2, 3, 4, [5, 6], 7, [4, [7, 9, 8]]]; // now i want that all the numbers to be in a single array .
+let another_arry = [1, 2, 2, 3, 4, [5, 6], 7, [4, [7, 9, 8]]]; // now i want that all the numbers to be in a single array .
 
-// let new_another_array = another_arry.flat(Infinity); // flat will merge all array and instead of infinity we an give the depth vale so that it will merge till where ;
+let new_another_array = another_arry.flat(Infinity); // flat will merge all array and instead of infinity we an give the depth value so that it will merge till where we want;
 
-// console.log(new_another_array);
+console.log(new_another_array);
 
 
-// console.log(Array.isArray("Rajnish")); // it will check weather it is array or not.
+console.log(Array.isArray("Rajnish")); // it will check weather it is array or not.
 
-// console.log(Array.from("Rajnish")); // ['R', 'a', 'j', 'n', 'i', 's','h']
+console.log(Array.from("Rajnish")); // ['R', 'a', 'j', 'n', 'i', 's','h']
 
-// console.log(Array.of("Rajnish")); // [ 'Rajnish' ]
+console.log(Array.of("Rajnish")); // [ 'Rajnish' ]
 
 let score1 = 100
 let score2 = 200
